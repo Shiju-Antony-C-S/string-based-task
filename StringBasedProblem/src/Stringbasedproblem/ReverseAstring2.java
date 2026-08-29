d97e7f7da6537e6ea57c68ad8f3777f6b4280151
Shiju Antony CS <Shiju Antony CS@CS>
2026-08-29 12:21:13 +0530

    commit

---
+++ StringBasedProblem/src/Stringbasedproblem/ReverseAstring2.java
@@ -0,0 +1,21 @@
+package Stringbasedproblem;
+import java.util.Scanner;
+public class ReverseAstring2 {
+	    static String reverseString(String str){
+	    String empStr="";
+	       for(int i=0;i<str.length();i++){
+	           int val = (str.charAt(i) - 97 )+1;
+	           int div = ((val%26)+1)+96;
+	           empStr+=((char)(div));
+	           
+	       }
+	       return empStr;
+	    }
+		public static void main(String[] args) {
+			System.out.print("Enter the sequence of character");
+			Scanner in = new Scanner(System.in);
+			String str = in.nextLine();
+			System.out.println(ReverseAstring2.reverseString(str));
+			in.close();
+			}
+	}
